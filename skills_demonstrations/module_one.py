a856f4879ea08dfb2b1772343b6445701a4f0a1c
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-11 10:00:16 -0700

    Completed module 1 skills demo

---
+++ skills_demonstrations/module_one.py
@@ -0,0 +1,8 @@
+'''
+CIS141 Module #1 Skills Demonstration
+1) Create a simple Python program that prints something to the screen
+2) Save in appropriate folder
+3) Test to make sure it works and fix any errors that come up
+'''
+print("Here is my skills demonstration for Module #1")
+print("Thank you!")
