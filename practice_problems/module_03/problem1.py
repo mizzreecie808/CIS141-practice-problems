af018d88622dc6c5b6bba80eb24baea574d8593a
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-13 10:11:02 -0700

    Updated file names

---
+++ practice_problems/module_03/problem1.py
@@ -0,0 +1,6 @@
+'''
+Problem #1
+Prompt the user for a word.
+Then, prompt the user for how many times they'd like that word repeated.
+Print the word the correct number of times.
+'''
---
+++ practice_problems/module_03/problem4.py
@@ -0,0 +1,6 @@
+'''
+Problem #4
+Prompt the user for: a word, a first index, and a last index. 
+Slice the word at the indexes provided by the user
+Print to the screen.
+'''
