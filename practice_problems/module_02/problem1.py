103c020d97a0cd9a8e4a6b96a3e197a7a1f40bff
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-14 09:07:35 -0700

    Completed Module #2 Problem #1

--- practice_problems/module_02/problem1.py
+++ practice_problems/module_02/problem1.py
@@ -1,4 +1,5 @@
 '''
+Module #2 - Problem #1
 Create a program that prints the following output to the screen:
 "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony.
 Then, everything changed when the Fire Nation attacked."
