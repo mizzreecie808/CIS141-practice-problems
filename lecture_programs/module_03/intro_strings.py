b54fb73e650dafaaa26a993ec22b627bf72fe2f7
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-14 09:59:24 -0700

    Module #3 Intro to Strings

---
+++ lecture_programs/module_03/intro_strings.py
@@ -0,0 +1,13 @@
+# Introduction to Strings & Basic Operators
+my_str = "World"
+
+print(len(my_str))
+
+# Mixing quotes & escape character for backslash
+sentence_one = "She said, 'Python is fun!'"
+sentence_two = 'She said, "Python is fun!"'
+
+# sentence_three = "She said, "Python is fun!"" <-- error message thrown
+
+print(sentence_one)
+print(sentence_two)
