72ce02d3d5bd6ed5fc555cdf81a2409c9e645340
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-16 10:39:39 -0700

    Completed Module 03 practice problems

--- practice_problems/module_03/problem1.py
+++ practice_problems/module_03/problem1.py
@@ -4,3 +4,18 @@ Prompt the user for a word.
 Then, prompt the user for how many times they'd like that word repeated.
 Print the word the correct number of times.
 '''
+
+# prompt user for word
+word = input("Please type in a word of your choice.\t")
+
+# prompt for times to repeat, must change type to int
+repeat = int(input("Now tell me how many times you want to repeat this word.\t"))
+
+# this will print all words joined on one line
+print(word * repeat)
+
+# this will print the word on new lines
+print((word + "\n") * repeat)
+
+# this will print the word with space (or character of chosing) between each
+print((word + " ") * repeat)
--- practice_problems/module_03/problem2.py
+++ practice_problems/module_03/problem2.py
@@ -1,7 +1,25 @@
 '''
 Problem #2
 Prompt the user for their name and their age.
-Calculate their age next year. 
-Use string concatenation to print 
+Calculate their age next year.
+Use string concatenation to print
 "Hello, <name>! You are <age1> years old. Next year, you will be <age2> years old."
 '''
+
+# prompt user for name and age
+name = input("What is your name?  ")
+age = int(input("How old are you?  "))
+
+# calculate their age next year
+next_year = age + 1
+
+# concatenation
+statement = "Hello, " + name + "! you are " + str(age) + " years old. Next year, you will be " + str(next_year) + " years old."
+print(statement)
+
+# f-string allow variables to be embedded into expressions
+print(f"Hello, {name}! You are {age} years old. Next year, you will be {next_year} years old.")
+
+# .format() - not from course videos
+result = "Hello, {}! You are {} years old. Next year, you will be {} years old.".format(name, age, next_year)
+print(result)
