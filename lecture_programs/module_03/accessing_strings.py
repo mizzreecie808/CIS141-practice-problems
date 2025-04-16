cc0efc4eca900bfc39febb03ebe4fdfc2b5bb715
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-16 10:38:55 -0700

    Module 03 Lecture Videos

---
+++ lecture_programs/module_03/accessing_strings.py
@@ -0,0 +1,27 @@
+# Accessing & Slicing Strings & Comparison
+
+# Indexing, including out of range errors
+str = "Olympic College Rangers"
+print(str[10])
+
+length = len(str) - 1
+print(str[length])
+
+# Slicing & Stepping - Substring
+# Choose starting number and the ending number + 1
+slice = str[0:7]
+print(slice)
+
+slice2 = str[:7]
+print(slice2)
+
+end = str[16:]
+print(end)
+
+stepping = str[::3] #prints every nth character
+print(stepping)
+
+# String Comparison
+print("olympic" == "olympic") # True
+print("Olympic" == "olympic") # False
+print("Olympic" != "olympic") # True
