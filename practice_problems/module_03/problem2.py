5f031f47f926346c81cf97eff49270fb4576abbd
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-22 08:47:34 -0700

    Comleted Module 03 Problem2

--- practice_problems/module_03/problem2.py
+++ practice_problems/module_03/problem2.py
@@ -14,12 +14,10 @@ age = int(input("How old are you?  "))
 next_year = age + 1
 
 # concatenation
-statement = "Hello, " + name + "! you are " + str(age) + " years old. Next year, you will be " + str(next_year) + " years old."
+statement = "Hello, " + name + "! you are " + str(age) \
++ " years old. Next year, you will be " + str(next_year) + " years old."
 print(statement)
 
 # f-string allow variables to be embedded into expressions
-print(f"Hello, {name}! You are {age} years old. Next year, you will be {next_year} years old.")
-
-# .format() - not from course videos
-result = "Hello, {}! You are {} years old. Next year, you will be {} years old.".format(name, age, next_year)
-print(result)
+print(f"Hello, {name}! You are {age} years old. Next year, \
+you will be {next_year} years old.")
