8f6e7442a7fc99073715cbf431b592d2392df323
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-22 09:44:52 -0700

    Completed Module 03 Problem 5

--- practice_problems/module_03/problem5.py
+++ practice_problems/module_03/problem5.py
@@ -7,5 +7,8 @@ Use the appropriate keyword argument in print() to do so.
 # set the 3 words
 word1, word2, word3 = "Hello", "Everyone", "Amazing"
 
+# set the chosen seperation character
+pipe = "|"
+
 # print the 3 words with | between them
-print(word1, word2, word3, sep="|")
+print(word1, word2, word3, sep=pipe)
