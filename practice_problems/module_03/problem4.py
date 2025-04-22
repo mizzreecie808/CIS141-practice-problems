c68775041187fb2c26ad79333d30d2bc833c998e
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-22 09:41:50 -0700

    Update problem4.py
    Completed Module 03 Problem 4

--- practice_problems/module_03/problem4.py
+++ practice_problems/module_03/problem4.py
@@ -1,6 +1,16 @@
 '''
 Problem #4
-Prompt the user for: a word, a first index, and a last index. 
+Prompt the user for: a word, a first index, and a last index.
 Slice the word at the indexes provided by the user
 Print to the screen.
 '''
+
+# user input for word, first and last index
+word = input("What word would you like to slice? ")
+first, last = int(input("First slice? ")), int(input("Last slice? "))
+
+# slice the word
+sliced = word[first:last]
+
+# print the sliced word
+print(f"Your sliced word at index {first} and {last} is: {sliced}")
