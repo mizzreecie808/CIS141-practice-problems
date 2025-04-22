8fc9e50a0d9b90d7ebacde628eb96d02c36b645a
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-22 09:42:07 -0700

    Update problem5.py
    Completed Module 03 Problem5

--- practice_problems/module_03/problem5.py
+++ practice_problems/module_03/problem5.py
@@ -3,3 +3,9 @@ Problem #5
 Print 3 words with a | character (called a pipe) in between them.
 Use the appropriate keyword argument in print() to do so.
 '''
+
+# set the 3 words
+word1, word2, word3 = "Hello", "Everyone", "Amazing"
+
+# print the 3 words with | between them
+print(word1, word2, word3, sep="|")
