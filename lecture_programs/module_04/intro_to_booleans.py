ab71a7e0a51c278f89d6a4090a79b414dd11a019
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-28 09:26:02 -0700

    Watched Intro to Booleans

---
+++ lecture_programs/module_04/intro_to_booleans.py
@@ -0,0 +1,17 @@
+# Introduction to Booleans
+# https://youtu.be/q2fUYJ0faKw
+# Boolean Values
+# True  | 1 | On  | Yes
+# False | 0 | Off | No
+
+is_raining = True
+print(is_raining, type(is_raining)) # Output: True
+
+if is_raining:
+    print("It's raining men")
+
+is_sunny = False
+print(is_sunny) # Output: False
+
+if not is_sunny:
+    print("Stay inside")
