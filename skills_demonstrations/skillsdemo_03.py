6465b34e6effb32cd1929ea11d702db24636bee1
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-04-28 09:25:33 -0700

    Module 03 Skills Demo

--- skills_demonstrations/skillsdemo_03.py
+++ skills_demonstrations/skillsdemo_03.py
@@ -6,22 +6,21 @@ length = len(str)
 print(length)
 
 # Add a new line after the comma & display it
-# Find comma and use index # to slice string
-comma = str.find(",") + 1
-str1 = str[:comma] + "\n" + str[comma + 1:]
+str1 = "Code never lies,\ncomments sometimes do"
 print(str1)
 
 # Make every character in the string uppercase & display it
 print(str.upper())
 
-# Use slicing to grab just "Code never lies" out of the string
-# Find comma and use index # to get sub-string
-comma = str.find(",")
-sub_string = str[:comma]
+# Use slicing to grab just "Code never lies" out of the string 15
+sub_string = str[:15]
 print(sub_string)
 
 # Calculate the length of the above substring
-# Concatenate the length onto the end of the substring
-# Display it
 length = len(sub_string)
-print(sub_string, length)
+
+# Concatenate the length onto the end of the substring
+display = f"{sub_string}: {length}"
+
+# Display it
+print(display)
