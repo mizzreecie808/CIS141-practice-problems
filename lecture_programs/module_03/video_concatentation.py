dac225a4ae0d7c5993e8d990f8138fec6062806b
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-05 09:46:06 -0700

    Renamed files

---
+++ lecture_programs/module_03/video_concatentation.py
@@ -0,0 +1,25 @@
+# Concatenation, Repetition, and Mixing Types
+# https://www.youtube.com/watch?v=dbuHO9rxaaY
+
+# Concatentation (+)
+str1 = "Hello" + " " + "World"
+#print(str1)
+
+# Concatenation with different types
+score = str(500) #<-- turn int into string
+text = "Your score is "
+#print(text + score)
+
+# Repetition (*)
+str2 = "####====@@@@"
+#print(str2 * 8)
+
+# In Keyword, checks if character(s) are in a string
+str3 = "apple"
+#print("Word" in str1) #useful for booleans
+
+# Escape Sequences: \n, \t, \\, \", \'
+str4 = "Bond,\nJames Bond"
+str5 = "Handley,\tLindsey\nFoster,\tStephen\nBecker,\tRichard"
+#print(str4)
+print(str5)
