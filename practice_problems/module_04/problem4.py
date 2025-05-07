56bbdffdc8c8c9197bd1785c765b90290a74c1c5
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-07 09:37:06 -0700

    Allowing for ages 13, 17 and 18

--- practice_problems/module_04/problem4.py
+++ practice_problems/module_04/problem4.py
@@ -14,3 +14,18 @@ elif 13 < age < 17:
     print("You can watch G and PG-13 rated movies.")
 else:
     print("You can only watch G rated movies.")
+
+
+# Problem 4: you're currently skipping logic for ages 13 & 17.
+# Consider what should happen if someone inputs 13 or 17.
+# I added in the ">=" symbol for the if and elif statements.
+# This will allow for inputs of exactly 13, 17 and 18
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You can watch G, PG-13 and R rated movies.")
+elif 13 <= age <= 17:
+    print("You can watch G and PG-13 rated movies.")
+else:
+    print("You can only watch G rated movies.")
