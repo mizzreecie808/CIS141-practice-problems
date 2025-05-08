cae7c4184061afb3d21234e74b854f2ee60e8592
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-08 14:09:45 -0700

    Update video_break-v-continue.py

--- lecture_programs/module_05/video_break-v-continue.py
+++ lecture_programs/module_05/video_break-v-continue.py
@@ -1,15 +1,17 @@
 # Break vs. Continue
+# https://www.youtube.com/watch?v=YhgHdWrm930
 
 # Break: exits the loop (breaks you out!)
-#for i in range(10): #0 ... 9
-#    if i == 5:
-#        break
-#    print("i=",i)
-#print("Outside the for loop")
-
-# Continue: skips the current iteration of  loop (continues to next iteration)
-for i in range(10): #0 ... 9
+for i in range(10): # 0.....9
     if i == 5:
-        continue
-    print("i=",i)
+        print(f"i = {i} - Break!")
+        break       # sends us out of this for loop
+    print(f"i = {i}")
+print("Outside the for loop")
+
+# Continue: skips the current iteration of the loop (continues to next iteration)
+for i in range(10): # 0.....9
+    if i == 5:
+        continue      # skips 5
+    print(f"i = {i}")
 print("Outside the for loop")
