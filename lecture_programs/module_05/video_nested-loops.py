aa0acf05183471c1999a66cc5f27ac7be95a28c0
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-08 14:09:48 -0700

    Update video_nested-loops.py

--- lecture_programs/module_05/video_nested-loops.py
+++ lecture_programs/module_05/video_nested-loops.py
@@ -1,10 +1,11 @@
 # Nested Loops
+# https://www.youtube.com/watch?v=rUjN2WxvNbc
 
-#for i in range(3):           # Outer loop (0,1,2)
-#    for j in range(2):       # Inner loop (0,1)
-#        print(f"({i},{j})")
-#    print("Exiting inner loop")
-#print("Exiting outer loop")
+# for i in range(3):      # Outer loop (0, 1, 2)
+#     for j in range(2):  # Inner loop (0, 1)
+#         print(f"({i},{j})")
+#     print("Exiting inner loop")
+# print("Exiting outer loop")
 
 #1 2 3 4 5
 #1 2 3 4 5
@@ -12,7 +13,7 @@
 #1 2 3 4 5
 #1 2 3 4 5
 
-for i in range(5):           # Outer loop (0,1,2,3,4)
-    for j in range(5):       # Inner loop (0,1,2,3,4)
-        print(str(j + 1) + " ", end='')
+for i in range(5):          # Outer loop (0, 1, 2, 3, 4)
+    for j in range(5):      # Inner loop (0, 1, 2, 3, 4)
+        print(j + 1, end=" ")
     print("")
