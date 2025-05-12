0971d12fb58a12b3716f1e85084e8a6cad2ee9bc
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-12 10:51:32 -0700

    Create video-intro-lists.py

---
+++ lecture_programs/module_06/video-intro-lists.py
@@ -0,0 +1,23 @@
+# Lists: ordered collections of items (elements)
+# https://www.youtube.com/watch?v=v9CqPXNjHmw
+# my_list = [element1, element2, element3]
+
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "cherry"]
+mixed = [1, "two", 3.0]
+sublist = [[1, 2, 3], ["one", "two", "three"], ["I", "II", "III"]]
+
+# print(numbers)
+# print(sublist)
+# print(type(sublist))
+
+# Empty Lists
+empty = []
+# print(empty)
+
+# len()
+# print(len(numbers))
+
+# Indexing: starts at 0
+# print(fruits[0])
+print(sublist[1][2])
