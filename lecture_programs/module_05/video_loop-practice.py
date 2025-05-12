48e18b679ad6bd9f4902ada0551597a8db2b9c60
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-12 10:51:04 -0700

    Update video_loop-practice.py

--- lecture_programs/module_05/video_loop-practice.py
+++ lecture_programs/module_05/video_loop-practice.py
@@ -1,25 +1,26 @@
 # More Practice Problems
+# https://www.youtube.com/watch?v=ZyWbdMeN3oA
 
 # Problem 1: Write a while loop that sums numbers from 1 to 50.
-#counter = 1
-#sum = 0
-#while counter <= 50:
-#    sum += counter
-    #print("counter = ", counter, "; sum = ", sum)
-#    counter += 1
-#print(sum)
+# counter = 1
+# total = 0
 
-# Problem 2: Write a for loop that prints the even numbers from 1 to 20, but skip printing 10 using continue.
-#for i in range(1,21): #1 ... 20
-#    if i == 10:
-#        continue
-#    if i % 2 == 0: #Really awesome way to test if number is even
-#        print(i)
+# while counter < 51:
+#     total += counter
+#     print(f"counter = {counter}; sum = {total}")
+#     counter += 1
+# print(f"sum = {total}")
 
+# Problem 2: Write a for loop that prints the even numbers from 1 to 20,
+# but skip printing 10 using continue.
+# for i in range(1, 21):
+#     if i % 2 != 0 or i == 10:
+#         continue
+#     print(f"i = {i}")
 
-# Problem 3: Write a program that creates a pyramid of asterisks (stars) to the console.
-# For each row in the pyramid, you use one loop to print the required number of spaces and another loop to
-# print the required number of asterisks.
+# Problem 3: Write a program that creates a pyramid of asterisks (stars)
+# For each row in the pyramid, you use one loop to print the required number
+# of spaces and another loop to print the required number of asterisks.
 # Output should look like:
 #    *      Row 1: 4 spaces, 1 asterisk
 #   ***     Row 2: 3 spaces, 3 asterisks
@@ -28,12 +29,13 @@
 #*********  Row 5: 0 spaces, 9 asterisks
 
 row = 3
-for i in range(row): #0,1,2,3,4
-    # Loop for creating spaces
-    for _ in range(row - 1 - i):
-        print("_", end="")
-    # Loop for creating asterisks
-    for _ in range(1 + (2 * i)):
-        print("*", end="")
-    # Print a new line
-    print("")
+
+for i in range(row):                # 0, 1, 2, 3, 4
+    # loop for creating spaces
+    for j in range(row - 1 - i):
+        print(" ", end = "")
+    # loop for creating asterisks
+    for k in range(1 + (2 * i)):
+        print("*", end = "")
+    # print new line
+    print()
