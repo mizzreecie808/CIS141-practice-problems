c130f10bcf999b358255a75d032ac20fd458f275
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-14 10:30:47 -0700

    Create aibot.py

---
+++ lecture_programs/module_06/aibot.py
@@ -0,0 +1,27 @@
+# AI Bot Practice
+my_list = [7, 1, 2, 3, 4]
+# print(my_list)
+# my_list.insert(1, "hello")
+# print(my_list)
+
+# my_list.remove("hello")
+# print(my_list)
+
+# my_list.pop()
+# print(my_list)
+
+# removed_item = my_list.pop()  # removes last item
+# print(removed_item)
+
+# del my_list[1]    # Removes item at specified index
+# print(my_list)
+# my_list.sort()      # Sorts in place
+# print(my_list)
+# new_list = sorted(my_list)  # its a function and must take an input, creates a new list
+# print(my_list)
+# print(new_list)
+
+my_list.reverse()
+print(my_list)
+
+print(min(my_list))
