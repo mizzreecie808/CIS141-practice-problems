f61c0da0d6f1c689ffd9b194fee82bc11ebb8536
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-28 10:37:45 -0700

    Update practice.py

--- lecture_programs/module_06/practice.py
+++ lecture_programs/module_06/practice.py
@@ -1,2 +1,29 @@
 print(len("Wednesday, (60.0°F)"))
 print(len("--📋 Student Grades 📋--"))
+
+def sum_num(num1, num2):
+    return num1 + num2
+
+sum_num(5, 10)
+result = 15
+
+def sum_product(num1, num2):
+    total = num1 + num2
+    product = num1 * num2
+    return total, product
+
+total, product = sum_product(3, 4)
+print(total)
+print(product)
+
+def greet(name, greeting="Hello"):
+    return f"{greeting.capitalize()} {name.capitalize()}!"
+
+print(greet("cerise"))
+
+def circle_math(rad):
+    area = 3.14 * rad**2
+    circ = 2 * 3.14 * rad
+    return area, circ
+
+print(circle_math(3))
