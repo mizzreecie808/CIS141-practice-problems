b12b9aa4f2b8485257622aa1b60a16b185e299ed
mizzreecie808 <91648331+mizzreecie808@users.noreply.github.com>
2025-05-28 10:36:41 -0700

    Update video-scope.py

--- lecture_programs/module_07/video-scope.py
+++ lecture_programs/module_07/video-scope.py
@@ -1,7 +1,13 @@
 # Scope (Local vs. global)
 # https://www.youtube.com/watch?v=EzG5Vh9L_Qs
+# Scope refers to area of a program where a particular variable can be accessed
+# Pass by value: Pass an argument to a function. Sends a copy of that value, so
+# changes made to that parameter inside the function do not affect original variable.
+# Pass by Reference: Pass an argument to a function. Sends a reference to the
+# original variable, therefore modifying parameter inside function changes the
+# variable since they both point to the same memory location
 
-x = 10 #global variable
+# x = 10 #global variable
 def my_function():
     x = 5   # local variable, only accessed in this function
     y = 2   # local variable, only accessed in this function
@@ -24,6 +30,37 @@ num = 5
 def modify_list(lst):
     lst.append(4)   # Modifies original list
 
-numbers = [1, 2, 3]
-modify_list(numbers)
-print(numbers)
+# numbers = [1, 2, 3]
+# modify_list(numbers)
+# print(numbers)
+
+# https://www.youtube.com/watch?v=38uGbVYICwg
+# First indent is global
+# Nonlocal
+# x = 1
+# def foo():
+#     searches for variables local first, then global
+#     if you use global keyword, it will change
+#     global x
+#     x = 2
+#     y = 12
+#     print(f"foo sees x equal to {x}")
+#     print(f"foo sees y equal to {y}")
+# foo()
+# print(f"global sees x equal to {x}")
+
+x = 1
+def outer():
+    x = 2
+    def inner():
+        # changes the nonlocal variable x, one step above, not the global
+        nonlocal x
+        x = 3
+        print(f"inner sees x equal to {x}")
+        return
+    inner()
+    print(f"outer sees x equal to {x}")
+    return
+
+outer()
+print(f"global sees x equal to {x}")
